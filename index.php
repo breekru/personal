e201aa1ca17c2fb5ce54bbc24a5764736ad89175
breekru <breekru@gmail.com>
2022-07-06 17:03:43 -0500

    init

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+require('config.php');
+
+?>
+
+
+
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Exercise Challenge</title>
+  <meta name="description" content="Brett's Exercise Challenge">
+  <meta name="author" content="Brett Krumholz">
+
+  <meta property="og:title" content="Brett's Exercise Challenge">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="https://www.sitepoint.com/a-basic-html5-template/">
+  <meta property="og:description" content="Brett's Exercise Challenge">
+  <meta property="og:image" content="image.png">
+
+  <link rel="icon" href="img/favicon.ico">
+  <link rel="icon" href="img/favicon.svg" type="image/svg+xml">
+  <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+
+  <link rel="stylesheet" href="css/stats.css">
+
+</head>
+
+<body>
+<h1>Brett's Exercise Challenge</h1>
+<div class="stats">
+<?php
+$sql
+</div>
+
+
+</body>
+</html>
